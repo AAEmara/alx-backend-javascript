881d0f650807f411da0e20d5a5f140df4d2c1092
AAEmara <abdelrahmanemara18@gmail.com>
2024-05-28 01:39:54 +0000

    0x00: Task8 JavaScript Script - Ouput Fix.

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,8 +5,8 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    [`income-${getCurrentYear()}`]: gdp,
-    [`gdp-${getCurrentYear()}`]: income,
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
   };
 
