9d33e5d7f85ef102ddf3776b48d79373bc7f4fe7
AAEmara <abdelrahmanemara18@gmail.com>
2024-05-28 00:46:10 +0000

    0x00: Task8 JavaScript Script - Computed Property.

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: gdp,
+    [`gdp-${getCurrentYear()}`]: income,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
