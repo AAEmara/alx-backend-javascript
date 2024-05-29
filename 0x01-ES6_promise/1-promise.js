46c2c07fa23b1a6e2817c9d004cb2ad97a527d10
AAEmara <abdelrahmanemara18@gmail.com>
2024-05-29 11:54:32 +0000

    0x01: Task1 JavaScript Script - Resolve, Reject.

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else if (success === false) {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
+
+export default getFullResponseFromAPI;
