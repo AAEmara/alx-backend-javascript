99ad61af9102a384ecca435c925ca861d633af6b
AAEmara <abdelrahmanemara18@gmail.com>
2024-05-30 00:25:53 +0000

    0x01: Task3 JavaScript Script - Checker Fix.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} \
+${values[1].lastName}`);
+    })
     .catch(() => console.error('Signup system offline'));
 }
 
