a0122ca2f768cf072d394ace29df74904fc63818
AAEmara <abdelrahmanemara18@gmail.com>
2024-05-30 00:59:43 +0000

    0x01: Task6 JavaScript Script - Promise.allSettled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled(([signUpUser(firstName, lastName)], [uploadPhoto(fileName)]))
+    .then((results) => results);
+}
+
+export default handleProfileSignup;
