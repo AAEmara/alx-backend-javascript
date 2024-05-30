69ff6e015d6d38e31816b0cfcd1c577161287f21
AAEmara <abdelrahmanemara18@gmail.com>
2024-05-30 00:31:24 +0000

    0x01: Task3 JavaScript Script - Checker Fix 3.
    used the log instead of error method for the
    console class.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,7 +7,7 @@ function handleProfileSignup() {
       console.log(`${values[0].body} ${values[1].firstName} \
 ${values[1].lastName}`);
     })
-    .catch(() => console.error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
