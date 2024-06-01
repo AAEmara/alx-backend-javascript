3c35116c7553768a58ab5f56ac827b08d3f155a6
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-01 01:32:33 +0000

    0x01: Task6 JavaScript Script - All Promises.

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled(([signUpUser(firstName, lastName)], [uploadPhoto(fileName)]))
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled(([signUpUser(firstName, lastName), uploadPhoto(fileName)]))
     .then((results) => results);
 }
-
-export default handleProfileSignup;
