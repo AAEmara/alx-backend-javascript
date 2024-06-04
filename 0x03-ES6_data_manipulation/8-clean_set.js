7ca1d874026fdb25a11958bdf875fa6d74ca6a3e
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-04 08:14:06 +0000

    0x03: Task8 JavaScript Script - Fix Output 3.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,22 @@
 export default function cleanSet(set, startString) {
   const stringOutput = [];
+
   set.forEach((value) => {
-    if (value.startsWith(startString) && startString !== '') {
-      stringOutput.push(value.slice(startString.length));
+    // Checking if at least the length of the value equals the input string.
+    if (value.length >= startString.length) {
+      let match = true;
+      // Checking if the input string is matching the start of the value.
+      for (let i = 0; i < startString.length; i += 1) {
+        if (startString[i] !== value[i]) {
+          match = false;
+          break;
+        }
+      }
+
+      // Add to the array if the input string's length isn't 0 and it matches.
+      if (match && startString.length !== 0) {
+        stringOutput.push(value.slice(startString.length));
+      }
     }
   });
 
