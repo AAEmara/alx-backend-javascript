9510ffd09f68421685ab265a34cda9460e0e3fcd
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-26 12:58:26 +0300

    0x05: Task2 Node.js Script - Reading a CSV File.
    Read a CSV file and printed some insights about
    the data inside it.

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+// Including fs module.
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const fileContents = fs.readFileSync(path, 'utf8');
+    const lines = fileContents.split('\n').slice(1, -1);
+    const csvObjects = {};
+    for (const line of lines) {
+      if (line.trim()) {
+        // Splitting the fields of the current row.
+        const columns = line.split(',');
+        // Retrieving the field name.
+        const fieldName = columns[3];
+        if (!csvObjects[fieldName]) {
+          csvObjects[fieldName] = [];
+          csvObjects[fieldName][1] = [];
+          csvObjects[fieldName][0] = 0;
+        }
+        csvObjects[fieldName][0] += 1;
+        csvObjects[fieldName][1].push(columns[0]);
+      }
+    }
+    let count = 0;
+    for (const line of lines) {
+      if (line) {
+        count += 1;
+      }
+    }
+    console.log(`Number of students: ${count}`);
+    for (const key in csvObjects) {
+      if (key in csvObjects) {
+        const names = csvObjects[key][1].join(', ');
+        console.log(`Number of students in ${key}: \
+${csvObjects[key][0]}. \
+List: ${names}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
