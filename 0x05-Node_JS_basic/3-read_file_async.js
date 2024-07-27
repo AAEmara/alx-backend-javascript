b648e7b7b6a4383dd9805bdba7cef274804a3b74
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-27 03:39:25 +0300

    0x05: Task3 Node.js Script - readFile with Promise
    Used an asynchronous method to read the file
    contents.

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,47 @@
+// Including readFile method from fsPromise module.
+const { readFile } = require('node:fs/promises');
+
+async function countStudents(path) {
+  try {
+    // Reading the database file asynchronously.
+    const fileContents = await readFile(path, { encoding: 'utf8' });
+    const lines = fileContents.split('\n').slice(1, -1);
+    const csvObjects = {};
+    for (const line of lines) {
+      if (line.trim()) {
+        // Splitting the fields of the current row.
+        const columns = line.split(',');
+        // Retrieving the field name.
+        const fieldName = columns[3];
+        if (!csvObjects[fieldName]) {
+          csvObjects[fieldName] = [];
+          csvObjects[fieldName][1] = [];
+          csvObjects[fieldName][0] = 0;
+        }
+        csvObjects[fieldName][0] += 1;
+        csvObjects[fieldName][1].push(columns[0]);
+      }
+    }
+    let count = 0;
+    for (const line of lines) {
+      if (line.trim()) {
+        count += 1;
+      }
+    }
+    console.log(`Number of students: ${count}`);
+    for (const key in csvObjects) {
+      if (key in csvObjects) {
+        const names = csvObjects[key][1].join(', ');
+        console.log(`Number of students in ${key}: \
+${csvObjects[key][0]}. \
+List: ${names}`);
+      }
+    }
+
+    return (fileContents);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
